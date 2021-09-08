d329a6b5a27877b106e6373504a0617a6d8743ac
Azuri <>
2021-09-08 15:10:49 -0400

    Fix echo lol

--- update_java.sh
+++ update_java.sh
@@ -7,4 +7,4 @@ else
 fi
 
 $COMPOSE build --pull $@
-echo "Don't forget to stop the server and `docker compose up -d` again if there was a new java version."
+echo "Don't forget to stop the server and run \`docker compose up -d\` again if there was a new java version."
