987407386703d80dc01fa4fb873220953f01ab50
Azuri <>
2021-09-08 15:07:41 -0400

    Add update java container script

---
+++ update_java.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if ! command -v docker-compose &> /dev/null
+then
+    COMPOSE="docker compose"
+else
+    COMPOSE="docker-compose"
+fi
+
+$COMPOSE build --pull $@
+echo "Don't forget to stop the server and `docker compose up -d` again if there was a new java version."
