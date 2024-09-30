0fd73493ab4d14075c175627c928c327dbbb2192
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2024-09-30 00:40:01 -0400

    Update modern toolchain for mc in 2024, mc v1.21.x

--- run_update_checker.sh
+++ run_update_checker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --env-file update_checker.env -v $(pwd):/root/src -w /root/src golang:1.17-bullseye sh -c "go run update_checker.go"
+docker run --rm --env-file update_checker.env -v $(pwd):/root/src -w /root/src golang:1.23-alpine sh -c "go run update_checker.go"
--- update_java.sh
+++ update_java.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-if ! command -v docker-compose &> /dev/null
-then
-    COMPOSE="docker compose"
-else
-    COMPOSE="docker-compose"
-fi
-
-$COMPOSE build --pull $@
+docker compose build --pull "$@"
 echo "Don't forget to stop the server and run \`docker compose up -d\` again if there was a new java version."
